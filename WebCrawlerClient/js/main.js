832fc22c8b975767243428a653d862a3f1ad94d6
justinjohn83 <jmontgomery.init@gmail.com>
2013-11-30 19:36:18 -0600

    main.js cleanup and added arrow head to show directed-ness of graph

--- WebCrawlerClient/js/main.js
+++ WebCrawlerClient/js/main.js
@@ -6,32 +6,42 @@
 
 (function($){
 
-  var Renderer = function(canvas){
-    var canvas = $(canvas).get(0)
+  var renderer = function(canvas){
+    var canvas = $(canvas).get(0);
     var ctx = canvas.getContext("2d");
-    var particleSystem
+    var particleSystem;
 
-    var that = {
-      init:function(system){
+    var that = {};
+     that.init = function(system){
         //
         // the particle system will call the init function once, right before the
         // first frame is to be drawn. it's a good place to set up the canvas and
         // to pass the canvas size to the particle system
         //
         // save a reference to the particle system for use in the .redraw() loop
-        particleSystem = system
+        particleSystem = system;
 
         // inform the system of the screen dimensions so it can map coords for us.
         // if the canvas is ever resized, screenSize should be called again with
         // the new dimensions
-        particleSystem.screenSize(canvas.width, canvas.height) 
-        particleSystem.screenPadding(80) // leave an extra 80px of whitespace per side
+        particleSystem.screenSize(canvas.width, canvas.height) ;
+        particleSystem.screenPadding(80); // leave an extra 80px of whitespace per side
         
         // set up some event handlers to allow for node-dragging
-        that.initMouseHandling()
-      },
+        that.initMouseHandling();
+      };
       
-      redraw:function(){
+      that.redraw = function(){
+    	  
+    	// length of head in pixels
+	  function canvas_arrow(ctx, fromx, fromy, tox, toy,headlen){
+		    var angle = Math.atan2(toy-fromy,tox-fromx);
+		    ctx.moveTo(fromx, fromy);
+		    ctx.lineTo(tox, toy);
+		    ctx.lineTo(tox-headlen*Math.cos(angle-Math.PI/6),toy-headlen*Math.sin(angle-Math.PI/6));
+		    ctx.moveTo(tox, toy);
+		    ctx.lineTo(tox-headlen*Math.cos(angle+Math.PI/6),toy-headlen*Math.sin(angle+Math.PI/6));
+		}  
         // 
         // redraw will be called repeatedly during the run whenever the node positions
         // change. the new positions for the nodes can be accessed by looking at the
@@ -41,8 +51,8 @@
         // which allow you to step through the actual node objects but also pass an
         // x,y point in the screen's coordinate system
         // 
-        ctx.fillStyle = "white"
-        ctx.fillRect(0,0, canvas.width, canvas.height)
+        ctx.fillStyle = "white";
+        ctx.fillRect(0,0, canvas.width, canvas.height);
         
         particleSystem.eachEdge(function(edge, pt1, pt2){
           // edge: {source:Node, target:Node, length:#, data:{}}
@@ -50,26 +60,29 @@
           // pt2:  {x:#, y:#}  target position in screen coords
 
           // draw a line from pt1 to pt2
-          ctx.strokeStyle = "rgba(0,0,0, .333)"
-          ctx.lineWidth = 1
-          ctx.beginPath()
-          ctx.moveTo(pt1.x, pt1.y)
-          ctx.lineTo(pt2.x, pt2.y)
-          ctx.stroke()
-        })
+          ctx.strokeStyle = "rgba(0,0,0, .333)";
+          ctx.lineWidth = 1;
+          ctx.beginPath();
+          
+          canvas_arrow(ctx,pt1.x,pt1.y,pt2.x,pt2.y,15);
+
+         // ctx.moveTo(pt1.x, pt1.y);
+         // ctx.lineTo(pt2.x, pt2.y);
+          ctx.stroke();
+        });
 
         particleSystem.eachNode(function(node, pt){
           // node: {mass:#, p:{x,y}, name:"", data:{}}
           // pt:   {x:#, y:#}  node position in screen coords
 
           // draw a rectangle centered at pt
-          var w = 10
-          ctx.fillStyle = (node.data.alone) ? "orange" : "black"
-          ctx.fillRect(pt.x-w/2, pt.y-w/2, w,w)
-        })    			
-      },
+          var w = 10;
+          ctx.fillStyle = (node.data.alone) ? "orange" : "black";
+          ctx.fillRect(pt.x-w/2, pt.y-w/2, w,w);
+        }); 			
+      };
       
-      initMouseHandling:function(){
+      that.initMouseHandling = function(){
         // no-nonsense drag and drop (thanks springy.js)
         var dragged = null;
 
@@ -78,63 +91,62 @@
         var handler = {
           clicked:function(e){
             var pos = $(canvas).offset();
-            _mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
+            _mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top);
             dragged = particleSystem.nearest(_mouseP);
 
             if (dragged && dragged.node !== null){
               // while we're dragging, don't let physics move the node
-              dragged.node.fixed = true
+              dragged.node.fixed = true;
             }
 
-            $(canvas).bind('mousemove', handler.dragged)
-            $(window).bind('mouseup', handler.dropped)
+            $(canvas).bind('mousemove', handler.dragged);
+            $(window).bind('mouseup', handler.dropped);
 
-            return false
+            return false;
           },
           dragged:function(e){
             var pos = $(canvas).offset();
-            var s = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
+            var s = arbor.Point(e.pageX-pos.left, e.pageY-pos.top);
 
             if (dragged && dragged.node !== null){
-              var p = particleSystem.fromScreen(s)
-              dragged.node.p = p
+              var p = particleSystem.fromScreen(s);
+              dragged.node.p = p;
             }
 
-            return false
+            return false;
           },
 
           dropped:function(e){
-            if (dragged===null || dragged.node===undefined) return
-            if (dragged.node !== null) dragged.node.fixed = false
-            dragged.node.tempMass = 1000
-            dragged = null
-            $(canvas).unbind('mousemove', handler.dragged)
-            $(window).unbind('mouseup', handler.dropped)
-            _mouseP = null
-            return false
+            if (dragged===null || dragged.node===undefined) return;
+            if (dragged.node !== null) dragged.node.fixed = false;
+            dragged.node.tempMass = 1000;
+            dragged = null;
+            $(canvas).unbind('mousemove', handler.dragged);
+            $(window).unbind('mouseup', handler.dropped);
+            _mouseP = null;
+            return false;
           }
         }
         
         // start listening
         $(canvas).mousedown(handler.clicked);
 
-      },
+      };
       
-    }
-    return that
-  }    
+    return that;
+  };   
 
   $(document).ready(function(){
-    var sys = arbor.ParticleSystem(1000, 600, 0.5) // create the system with sensible repulsion/stiffness/friction
-    sys.parameters({gravity:true}) // use center-gravity to make the graph settle nicely (ymmv)
-    sys.renderer = Renderer("#viewport") // our newly created renderer will have its .init() method called shortly by sys...
+    var sys = arbor.ParticleSystem(1000, 600, 0.5); // create the system with sensible repulsion/stiffness/friction
+    sys.parameters({gravity:true}); // use center-gravity to make the graph settle nicely (ymmv)
+    sys.renderer = renderer("#viewport"); // our newly created renderer will have its .init() method called shortly by sys...
 
     // add some nodes to the graph and watch it go...
-    sys.addEdge('a','b')
-    sys.addEdge('a','c')
-    sys.addEdge('a','d')
-    sys.addEdge('a','e')
-    sys.addNode('f', {alone:true, mass:.25})
+    sys.addEdge('a','b');
+    sys.addEdge('a','c');
+    sys.addEdge('a','d');
+    sys.addEdge('a','e');
+    sys.addNode('f', {alone:true, mass:.25});
 
     // or, equivalently:
     //
@@ -151,6 +163,6 @@
     //   }
     // })
     
-  })
+  });
 
-})(this.jQuery)
+})(this.jQuery);
